5af13822aebc2209023bc5ceb640272225324463
Haykuhi Mkrtchyan <113100671+HaykuhiMk@users.noreply.github.com>
2023-06-15 23:40:11 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,62 +1,27 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <cctype>
+#include <aspell.h>
+#include <poppler/cpp/poppler-document.h>
+#include <poppler/cpp/poppler-page.h>
 #include <fstream>
 #include <sstream>
-#include <unordered_map>
-#include <vector>
-#include "invertedIndex.h"
+#include <algorithm>
+#include "text.h"
+#include "textReader.h"
+#include "pdfReader.h"
 
-void addToInvertedIndex(std::unordered_map<std::string, std::vector<std::string> >& invertedIndex, const std::string& word, const std::string& fileName)
-{
-    std::string normalizedWord = word;
-    auto it = invertedIndex.find(normalizedWord);
-    if (it != invertedIndex.end())
-    {
-        it->second.push_back(fileName);
-    }
-    else
-    {
-        invertedIndex.insert(std::make_pair(normalizedWord, std::vector<std::string>{fileName}));
-    }
-}
-
-void toInvInd()
-{
-    std::ifstream inFile("db.txt");
-    if (!inFile)
-    {
-        std::cerr << "Failed to open the input file." << std::endl;
-        return;
-    }
-
-    std::unordered_map<std::string, std::vector<std::string> > invertedIndex;
-
-    std::string line;
-    while (std::getline(inFile, line))
-    {
-        std::istringstream iss(line);
-        std::string word;
-        std::string fileName;
-        if (iss >> word >> fileName)
-        {
-            addToInvertedIndex(invertedIndex, word, fileName);
-        }
-    }
-
-    inFile.close();
-
-    for (const auto& entry : invertedIndex)
-    {
-        std::cout << entry.first << ": ";
-        for (const auto& fileName : entry.second)
-        {
-            std::cout << fileName << " ";
-        }
-        std::cout << std::endl;
-    }
-}
 
 int main()
 {
-    toInvInd();
+    PDFReader pdf;
+    pdf.readFile(1.pdf);
+    // TextReader text;
+    // text.readFile("1.txt");
+    // text.readFile("2.txt");
+    // text.searchWord("solve");
+    // text.print();
+
     return 0;
 }
